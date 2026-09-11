24cfe6841c050c4285d6e6f6f4691453842896bb
smyoon <ysm35@yuhan.ac.kr>
2026-09-11 10:55:59 +0900

    feat: load AI character animation styles

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import './mobile-refresh.css'
 import './commercial-refresh.css'
 import './product-v2.css'
+import './character-animation.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
